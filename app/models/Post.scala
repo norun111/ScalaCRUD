f704c65205a39b4f14ef18401f4b4ffcb955a6df
tomoyaueno29 <u.t012807@gmail.com>
2020-07-17 13:12:55 +0900

    Update Post.scala

--- app/models/Post.scala
+++ app/models/Post.scala
@@ -5,12 +5,13 @@ import org.joda.time.DateTime
 
 case class Post(
                     id: Long,
-                    userId:  Option[Long],
+                    userId:  Option[Long] = None,
                     text: String,
                     commentCount: Int,
-                    postedAt: DateTime ) {
+                    postedAt: DateTime )
 
-  def save()(implicit session: DBSession = Post.autoSession): Post.save(this)(session)
+object Post extends SQLSyntaxSupport[Post] {
+
 }
 
 
