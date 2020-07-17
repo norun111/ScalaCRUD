646e6a9845c1cac0ce1cb2cccaeab40e44cc5300
tomoyaueno29 <u.t012807@gmail.com>
2020-07-17 14:59:36 +0900

    Update PostController.scala

--- app/controllers/PostController.scala
+++ app/controllers/PostController.scala
@@ -1,13 +1,11 @@
 package controllers
 
-import javax.inject.{Inject, Singleton}
-import play.api.mvc.{AbstractController, ControllerComponents}
+import javax.inject.{ Inject, Singleton }
+import play.api.mvc.{ AbstractController, ControllerComponents }
 import scalikejdbc._
 import play.api.data._
 import play.api.data.Forms._
 import models._
-
-
 @Singleton
 class PostController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
