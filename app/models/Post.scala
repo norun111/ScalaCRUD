f24178e283eb603e4885440d68e6ad7a1478404b
tomoyaueno29 <u.t012807@gmail.com>
2020-07-17 14:59:39 +0900

    Update Post.scala

--- app/models/Post.scala
+++ app/models/Post.scala
@@ -1,26 +1,23 @@
 package models
 
+import java.time.ZonedDateTime
 import scalikejdbc._
-import org.joda.time.DateTime
 
-case class Post(
-                    id: Long,
-                    userId:  Option[Long] = None,
-                    user: Option[User] = None,
-                    text: String,
-                    commentCount: Int,
-                    postedAt: DateTime ) {
-
-}
+case class Post(id: Long,
+                userId: Option[Long] = None,
+                user: Option[User] = None,
+                text: String,
+                commentCount: Int,
+                postedAt: ZonedDateTime)
 
 object Post extends SQLSyntaxSupport[Post] {
   def apply(p: SyntaxProvider[Post])(rs: WrappedResultSet): Post = apply(p.resultName)(rs)
   def apply(p: ResultName[Post])(rs: WrappedResultSet): Post = new Post(
-    id = rs.get(p.id),
-    userId = rs.get(p.userId),
-    text = rs.get(p.text),
-    commentCount = rs.get(p.commentCount),
-    postedAt = rs.get(p.postedAt)
+    id = rs.long(p.id),
+    userId = rs.longOpt(p.userId),
+    text = rs.string(p.text),
+    commentCount = rs.int(p.commentCount),
+    postedAt = rs.zonedDateTime(p.postedAt)
   )
 
   // join query with company table
@@ -32,17 +29,35 @@ object Post extends SQLSyntaxSupport[Post] {
   private val u = User.u
   val p = Post.syntax("p")
 
+  // find by primary key
+//  def find(id: Long)(implicit session: DBSession = autoSession): Option[Post] =
+//    withSQL {
+//      select
+//        .from(Post as p)
+//        .leftJoin(User as u)
+//        .on(p.userId, u.id)
+//        .where
+//        .eq(p.id, id)
+//        .and
+//        .append(isNotDeleted)
+//    }.one(Post(p, u))
 
-  def create(userId: Option[Long] = None, text: String, commentCount: Int, postedAt: DateTime = DateTime.now)(implicit session: DBSession = autoSession): Post = {
+  def create(userId: Option[Long] = None,
+             text: String,
+             commentCount: Int,
+             postedAt: ZonedDateTime = ZonedDateTime.now)(implicit session: DBSession = autoSession): Post = {
     if (userId.isDefined && User.find(userId.get).isEmpty) {
       throw new IllegalArgumentException(s"User is not found. (userId: ${userId})")
     }
     val id = withSQL {
-      insert.into(Post).namedValues(
-        column.userId -> userId,
-        column.text -> text,
-        column.commentCount -> commentCount,
-        column.postedAt -> postedAt,
+      insert
+        .into(Post)
+        .namedValues(
+          column.userId -> userId,
+          column.text -> text,
+          column.commentCount -> commentCount,
+          column.postedAt -> postedAt
+        )
     }.updateAndReturnGeneratedKey.apply()
 
     Post(
@@ -50,8 +65,7 @@ object Post extends SQLSyntaxSupport[Post] {
       userId = userId.flatMap(id => User.find(id)),
       text = text,
       commentCount = commentCount,
-      postedAt = postedAt)
+      postedAt = postedAt
+    )
   }
 }
-
-
