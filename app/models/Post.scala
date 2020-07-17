d3e1f6ce66d6b46aacfceec499294acbbbac337f
tomoyaueno29 <u.t012807@gmail.com>
2020-07-17 13:44:04 +0900

    Update Post.scala

--- app/models/Post.scala
+++ app/models/Post.scala
@@ -6,6 +6,7 @@ import org.joda.time.DateTime
 case class Post(
                     id: Long,
                     userId:  Option[Long] = None,
+                    user: Option[User] = None,
                     text: String,
                     commentCount: Int,
                     postedAt: DateTime )
@@ -20,7 +21,35 @@ object Post extends SQLSyntaxSupport[Post] {
     postedAt = rs.get(p.postedAt)
   )
 
+  // join query with company table
+  def apply(p: SyntaxProvider[Post], u: SyntaxProvider[User])(rs: WrappedResultSet): Post = {
+    apply(p.resultName)(rs).copy(user = rs.longOpt(u.resultName.id).flatMap { _ =>
+      if (rs.timestampOpt(u.resultName.deletedAt).isEmpty) Some(User(u)(rs)) else None
+    })
+  }
+
   val p = Post.syntax("p")
+  private val u = User.u
+
+  def create(name: String, userId: Option[Long] = None, postedAt: DateTime = DateTime.now)(implicit session: DBSession = autoSession): Post = {
+    if (userId.isDefined && User.find(userId.get).isEmpty) {
+      throw new IllegalArgumentException(s"User is not found. (userId: ${userId})")
+    }
+    val id = withSQL {
+      insert.into(Post).namedValues(
+        column.userId -> userId,
+        column.text -> text,
+        column.commentCount -> commentCount,
+        column.postedAt -> postedAt)
+    }.updateAndReturnGeneratedKey.apply()
+
+    Post(
+      id = id,
+      userId = userId.flatMap(id => User.find(id)),
+      text = text,
+      commentCount = commentCount,
+      postedAt = postedAt)
+  }
 }
 
 
