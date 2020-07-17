60fbdeb7c43a09e44cd4a496fe9ce32521fed4dc
tomoyaueno29 <u.t012807@gmail.com>
2020-07-17 12:21:12 +0900

    Update 1.sql

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,7 +2,7 @@
 
 # --- !Ups
 
-CREATE TABLE users (
+CREATE TABLE User (
     id varchar(36) NOT NULL,
     name varchar(255) NOT NULL,
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -14,11 +14,27 @@ SET @user_1 = '11111111-1111-1111-1111-111111111111';
 SET @user_2 = '22222222-2222-2222-2222-222222222222';
 SET @user_3 = '33333333-3333-3333-3333-333333333333';
 
-INSERT INTO users(id, name) VALUES
+INSERT INTO User(id, name) VALUES
      (@user_1, 'alice')
     ,(@user_2, 'bob')
     ,(@user_3, 'charlie');
 
 # --- !Downs
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS User;
+
+# Add Post
+
+# --- !Ups
+CREATE TABLE Post (
+    id bigint(20) NOT NULL,
+    user_id bigint(20) NOT NULL,
+    text text NOT NULL,
+    comment_count int NOT NULL,
+    posted_at date NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES User(id),
+    PRIMARY KEY (id)
+);
+
+# --- !Downs
+DROP TABLE IF EXISTS Post;
