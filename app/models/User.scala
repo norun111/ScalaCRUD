8ee3efe20969d7eba50aa2b177fa94d97d65c3bc
tomoyaueno29 <u.t012807@gmail.com>
2020-07-18 19:11:16 +0900

    Update User.scala

--- app/models/User.scala
+++ app/models/User.scala
@@ -6,60 +6,46 @@ import scalikejdbc.config._
 
 case class User(id: Long, name: String)
 
-object User extends SQLSyntaxSupport[User] {
 
-  override val tableName = "user"
-
-  //  def apply(u: ResultName[User])(rs: WrappedResultSet): User = {
-  //    new User(
-  //      rs.long(u.id),
-  //      rs.string(u.name)
-  //    )
-  //  }
-}
-
-/*
 object User {
   def apply(rs: WrappedResultSet) = new User(rs.long("id"), rs.string("name"))
 }
 
-object Post {
-  def apply(rs: WrappedResultSet) = new Post(
-    rs.long("id"),
-    rs.string("text"),
-    rs.longOpt("user_id"),
-    rs.int("commentCount"),
-    rs.date("posted_at")
-  )
-
-  def withUser(rs: WrappedResultSet) = {
-    new Post(
-      id = rs.long("p_id"),
-      text = rs.string("p_text"),
-      userId = rs.longOpt("p_user_id"),
-      commentCount = rs.int("commentCount"),
-      posted_at = rs.date("posted_at"),
-      user = rs.longOpt("p_id").map(id => User(id, rs.string("u_name")))
-    )
-  }
-
-  def addPost(text: String): Unit = {
-    DBs.setupAll()
-
-    val count = DB autoCommit { implicit session =>
-      sql"insert into post (name, created_at) values (${text}, current_date())".update.apply()
-    }
-
-    DBs.closeAll()
-  }
-
-  def execute(): Unit = {
-    // loaded from "db.default.*"
-    val postIds = DB readOnly { implicit session =>
-      sql"select id from public.post".map(_.long(1)).list.apply()
-    }
-
-  }
-}
-
- */
+//object Post {
+//  def apply(rs: WrappedResultSet) = new Post(
+//    rs.long("id"),
+//    rs.string("text"),
+//    rs.longOpt("user_id"),
+//    rs.int("commentCount"),
+//    rs.date("posted_at")
+//  )
+//
+//  def withUser(rs: WrappedResultSet) = {
+//    new Post(
+//      id = rs.long("p_id"),
+//      text = rs.string("p_text"),
+//      userId = rs.longOpt("p_user_id"),
+//      commentCount = rs.int("commentCount"),
+//      posted_at = rs.date("posted_at"),
+//      user = rs.longOpt("p_id").map(id => User(id, rs.string("u_name")))
+//    )
+//  }
+//
+//  def addPost(text: String): Unit = {
+//    DBs.setupAll()
+//
+//    val count = DB autoCommit { implicit session =>
+//      sql"insert into post (name, created_at) values (${text}, current_date())".update.apply()
+//    }
+//
+//    DBs.closeAll()
+//  }
+//
+//  def execute(): Unit = {
+//    // loaded from "db.default.*"
+//    val postIds = DB readOnly { implicit session =>
+//      sql"select id from public.post".map(_.long(1)).list.apply()
+//    }
+//
+//  }
+//}
