b7065ca2fc9d0cd853928fed9c1b05113c52123c
tomoyaueno29 <u.t012807@gmail.com>
2020-07-18 20:19:09 +0900

    Update Post.scala

--- app/models/Post.scala
+++ app/models/Post.scala
@@ -12,6 +12,7 @@ object Post {
 
   DBs.setupAll()
 
+  // no used
   def findAll: Seq[Post] = DB readOnly { implicit session =>
     sql"SELECT id, user_id, text, commented_count, posted_at FROM POST"
       .map { rs =>
@@ -31,83 +32,3 @@ object Post {
         .apply()
     }
 }
-
-//object Post extends SQLSyntaxSupport[Post] {
-//
-//  override val tableName = "post"
-//
-//  override val columns = Seq("id", "user_id", "text", "posted_at", "comment_count")
-//
-//  def apply(p: SyntaxProvider[Post])(rs: WrappedResultSet): Post = apply(p.resultName)(rs)
-//
-//  def apply(p: ResultName[Post])(rs: WrappedResultSet): Post = new Post(
-//    id = rs.get(p.id),
-//    text = rs.string(p.text),
-//    userId = rs.get(p.userId),
-//    commentCount = rs.int(p.commentCount),
-//    posted_at = rs.get(p.postedAt)
-//  )
-//
-////
-////  //define alias name of user and post table
-////  val p = Post.syntax("p")
-////
-////  override val autoSession = AutoSession
-////
-////  def find(id: Long)(implicit session: DBSession = autoSession): Option[Post] = {
-////    withSQL {
-////      select.from(Post as p).where.eq(p.id, id)
-////    }.map(Post(p.resultName)).single.apply()
-////  }
-////
-////  def findAll()(implicit session: DBSession = autoSession): List[Post] = {
-////    withSQL(select.from(Post as p)).map(Post(p.resultName)).list.apply()
-////  }
-//
-////  def create(userId: Option[Long] = None,
-////             text: String,
-////             commentCount: Int,
-////             )(implicit session: DBSession = autoSession): Post = {
-////    val id = withSQL {
-////      insert.into(Post).namedValues(
-////          column.userId -> userId,
-////          column.text -> text,
-////          column.commentCount -> commentCount,
-////        )
-////    }.updateAndReturnGeneratedKey.apply()
-////
-////    Post(
-////      id = id,
-////      userId = userId,
-////      text = text,
-////      commentCount = commentCount
-////    )
-////  }
-//
-////   join query with company table
-////    def apply(p: SyntaxProvider[Post], u: SyntaxProvider[User])(rs: WrappedResultSet): Post = {
-////      apply(p.resultName)(rs).copy(user = rs.longOpt(u.resultName.id).flatMap { _ =>
-////        if (rs.timestampOpt(u.resultName.deletedAt).isEmpty) Some(User(u)(rs)) else None
-////      })
-////    }
-//
-////
-////  def opt(m: SyntaxProvider[Post])(rs: WrappedResultSet): Option[Post] =
-////    rs.longOpt(m.resultName.id).map(_ => Post(m)(rs))
-////
-//}
-/*
-  find by primary key
-  def find(id: Long)(implicit session: DBSession = autoSession): Option[Post] =
-    withSQL {
-      select
-        .from(Post as p)
-        .leftJoin(User as u)
-        .on(p.userId, u.id)
-        .where
-        .eq(p.id, id)
-        .and
-        .append(isNotDeleted)
-    }.one(Post(p, u))
-
- */
