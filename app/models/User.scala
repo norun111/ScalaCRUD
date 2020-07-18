3a4554194067626adb7b7c28e6111ec4cf320b5a
tomoyaueno29 <u.t012807@gmail.com>
2020-07-18 12:52:43 +0900

    Update User.scala

--- app/models/User.scala
+++ app/models/User.scala
@@ -1,12 +1,1 @@
-package models
 
-import scalikejdbc._
-
-case class User(id: Long, name: String)
-
-object User extends SQLSyntaxSupport[User] {
-
-  override val tableName = "user"
-
-  def apply(u: ResultName[User])(rs: WrappedResultSet) = new User(rs.long(u.id), rs.string(u.name))
-}
