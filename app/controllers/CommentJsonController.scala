90119c29a0989b243d15efc8593887baea72d9be
tomoyaueno29 <u.t012807@gmail.com>
2020-07-19 21:06:09 +0900

    Update CommentJsonController.scala

--- app/controllers/CommentJsonController.scala
+++ app/controllers/CommentJsonController.scala
@@ -1,5 +1,10 @@
 package controllers
 
-class CommentJsonController {
+import javax.inject.Inject
+import play.api.mvc.{AbstractController, ControllerComponents}
 
+class CommentJsonController @Inject()(components: ControllerComponents)
+  extends AbstractController(components) {
+
+  
 }
