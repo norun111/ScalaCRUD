3b3241980a9752f706bbe826999fd85c68c9fa6c
tomoyaueno29 <u.t012807@gmail.com>
2020-07-19 20:46:53 +0900

    Update Post.scala

--- app/models/Post.scala
+++ app/models/Post.scala
@@ -8,38 +8,24 @@ import scalikejdbc.config._
 
 case class Post(text: String, comment_count: Int)
 
-//QueryDSL
-//object Post extends SQLSyntaxSupport[Post] {
-//
-//  override val tableName = "post"
-//  override lazy val columns: Seq[String] = Seq("id", "text")
-//
-//  val p = Post.syntax("p")
-//  override val autoSession = AutoSession
-//
-//  def apply(p: SyntaxProvider[Post])(rs: WrappedResultSet): Post = apply(p.resultName)(rs)
-//  def apply(p: ResultName[Post])(rs: WrappedResultSet): Post = new Post(
-//    id = rs.string(p.id),
-//    text = rs.string(p.text),
-//  )
-//
-//}
-
 //SQLInterpolation
-object Post extends SQLSyntaxSupport[Post] {
+object Post extends SQLSyntaxSupport[User] {
 
   DBs.setupAll()
 
-  // no used
-  def findAll: Seq[Post] = DB readOnly { implicit session =>
-    sql"SELECT id, user_id, text, commented_count, posted_at FROM POST"
+  def findUserID(user_id: String): Option[User] = DB readOnly { implicit session =>
+    sql"""
+         SELECT id, name
+         FROM user
+         WHERE id = ${user_id}
+      """
       .map { rs =>
-        Post(
-          rs.string("text"),
-          rs.int("comment_count")
+        User(
+          id = rs.string("id"),
+          name = rs.string("name")
         )
       }
-      .list()
+      .single()
       .apply()
   }
 
@@ -49,7 +35,7 @@ object Post extends SQLSyntaxSupport[Post] {
              comment_count: Int,
              posted_at: Date): Unit =
     DB localTx { implicit session =>
-      sql"INSERT INTO post (id, user_id, text, comment_count, posted_at) VALUES (${id},${user_id},${text},${comment_count},${posted_at})"
+      sql"""INSERT INTO post (id, user_id, text, comment_count, posted_at) VALUES (${id},${user_id},${text},${comment_count},${posted_at})"""
         .update()
         .apply()
     }
