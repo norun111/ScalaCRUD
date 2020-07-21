145a8986639d222dbf148562a1fe4ce087e3200e
tomoyaueno29 <u.t012807@gmail.com>
2020-07-21 18:40:38 +0900

    Update Comment.scala

--- app/models/Comment.scala
+++ app/models/Comment.scala
@@ -41,7 +41,7 @@ object Comment {
         .apply()
     }
 
-  //コメント先のPostの情報を取得
+  //コメント先のPostの情報を検索
   def findPost(post_id: String = UUID.randomUUID.toString): Option[Post] =
     DB readOnly { implicit session =>
       sql"""
@@ -60,7 +60,28 @@ object Comment {
         }
         .single()
         .apply()
+    }
 
+  //コメント先のコメントを検索
+  def findComment(comment_id: String = UUID.randomUUID.toString): Option[Comment] =
+    DB readOnly { implicit session =>
+      sql"""
+         SELECT *
+         FROM comment
+         WHERE id = ${comment_id}
+      """
+        .map { rs =>
+          Comment(
+            id = rs.string("id"),
+            user_id = rs.string("user_id"),
+            text = rs.string("text"),
+            parent_post_id = rs.string("parent_post_id"),
+            comment_count = rs.int("comment_count"),
+            posted_at = rs.timestamp("posted_at")
+          )
+        }
+        .single()
+        .apply()
     }
 
   def create(id: String = UUID.randomUUID.toString,
@@ -94,6 +115,12 @@ object Comment {
     """.update().apply()
     }
 
-  def addComment(post_id: String = UUID.randomUUID.toString, comment_id: String = UUID.randomUUID.toString) =
+  //親Commentのコメント数を+1
+  def addCommentCountOnComment(comment_id: String = UUID.randomUUID.toString) =
+    DB autoCommit { implicit session =>
+      sql"""UPDATE comment SET comment_count = comment_count + 1
+    WHERE id = ${comment_id}
+    """.update().apply()
+    }
 
 }
