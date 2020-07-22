4884bda9da9c78e10178343b8da0c843c5b56499
tomoyaueno29 <u.t012807@gmail.com>
2020-07-22 20:05:05 +0900

    Update User.scala

--- app/models/User.scala
+++ app/models/User.scala
@@ -1,3 +1,24 @@
 package models
 
+import scalikejdbc._
+
 case class User(id: String, name: String)
+
+object User {
+  def findUser(user_id: String): Option[User] = DB readOnly { implicit session =>
+    sql"""
+         SELECT id, name
+         FROM user
+         WHERE id = ${user_id}
+      """
+      .map { rs =>
+        User(
+          id = rs.string("id"),
+          name = rs.string("name")
+        )
+      }
+      .single()
+      .apply()
+  }
+
+}
