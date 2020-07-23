befd521c06f219845b89d15b760bdd831687ca93
tomoyaueno29 <u.t012807@gmail.com>
2020-07-24 01:47:03 +0900

    Update Comment.scala

--- app/models/Comment.scala
+++ app/models/Comment.scala
@@ -16,9 +16,40 @@ case class Comment(
     posted_at: Date
 )
 
-object Comment {
+object Comment extends SQLSyntaxSupport[Comment] {
+
+  def apply(rs: WrappedResultSet): Comment = new Comment(
+    rs.string(c.id),
+    rs.string(c.user_id),
+    rs.string(c.text),
+    rs.string(c.parent_post_id),
+    rs.int(c.comment_count),
+    rs.date(c.posted_at)
+  )
 
   DBs.setupAll()
+
+  val c = Comment.syntax("c")
+
+  def allComments: Seq[Comment] =
+    DB readOnly { implicit session =>
+      sql"""
+         SELECT *
+         FROM comment
+      """
+        .map { rs =>
+          Comment(
+            id = rs.string("id"),
+            user_id = rs.string("user_id"),
+            text = rs.string("text"),
+            parent_post_id = rs.string("parent_post_id"),
+            comment_count = rs.int("comment_count"),
+            posted_at = rs.timestamp("posted_at")
+          )
+        }
+        .list()
+        .apply()
+    }
 
   def findAllComment(post_id: String = UUID.randomUUID.toString): Seq[CommentIndex] =
     DB readOnly { implicit session =>
@@ -84,12 +115,13 @@ object Comment {
     }
 
   //親Commentのコメント数を+1
-  def addCommentCountOnComment(comment_id: String = UUID.randomUUID.toString) =
+  def updateCommentCount(comment_id: String = UUID.randomUUID.toString) =
     DB autoCommit { implicit session =>
       sql"""UPDATE comment SET comment_count = comment_count + 1
     WHERE id = ${comment_id}
     """.update().apply()
     }
+
   /*
   // not used
   //値を0で挿入し、その後にpost.comment_count + 1でupdate
