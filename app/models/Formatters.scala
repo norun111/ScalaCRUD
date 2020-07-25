0ecefe63ba0551e5a5ac913961e3cb9dfdd2d72c
tomoyaueno29 <u.t012807@gmail.com>
2020-07-25 22:39:01 +0900

    Update Formatters.scala

--- app/models/Formatters.scala
+++ app/models/Formatters.scala
@@ -4,6 +4,7 @@ package models
 import play.api.libs.json._
 
 object Formatters {
+  implicit val nestCommentFormat = Json.format[nestComment]
   implicit val commentFormat = Json.format[Comment]
   implicit val postFormat = Json.format[Post]
 }
