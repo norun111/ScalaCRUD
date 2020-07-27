54c6e4145b66e5661862a350e7935925d8b313b0
tomoyaueno29 <u.t012807@gmail.com>
2020-07-27 20:22:21 +0900

    Update FormatterController.scala

--- app/controllers/FormatterController.scala
+++ app/controllers/FormatterController.scala
@@ -3,6 +3,7 @@ package controllers
 import models.{ nestComment, Comment, Post }
 import play.api.libs.json._
 
+// 全投稿一覧情報のJSONコンバーター
 object FormatterController {
   implicit val nestCommentFormat = Json.format[nestComment]
   implicit val commentFormat = Json.format[Comment]
