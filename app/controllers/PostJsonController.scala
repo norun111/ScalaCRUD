d0b3b8eca3cfe07b72bc5e6f93cf2d9c6b5da744
tomoyaueno29 <u.t012807@gmail.com>
2020-07-28 17:42:06 +0900

    Update PostJsonController.scala

--- app/controllers/PostJsonController.scala
+++ app/controllers/PostJsonController.scala
@@ -37,7 +37,7 @@ class PostJsonController @Inject()(components: ControllerComponents)
 
   //全投稿一覧を取得
   def index = Action { implicit request =>
-    val posts = Post.findAllPost
+    val posts = Post.findAllPosts
     Ok(Json.obj("posts" -> Json.toJson(posts)))
   }
 
@@ -54,11 +54,10 @@ class PostJsonController @Inject()(components: ControllerComponents)
                 //文字列長が0の状態
                 BadRequest(
                   Json.obj("meta" -> Json.toJson(Meta(400, "Can't be registered with null text"))))
-//                BadRequest((Json.toJson(Response(Meta(400, "Can't be registered with null text")))))
               } else if (form.text.length > 100) {
                 //文字列長が100より長い状態
-                BadRequest((Json.toJson(
-                  Response(Meta(400, "Can't be registered with more than 100 characters")))))
+                BadRequest(
+                  Json.obj("meta" -> (Json.toJson(Meta(400, "Can't be registered with more than 100 characters")))))
               } else {
                 val uuid = UUID.randomUUID
                 Post.create(uuid.toString, form.user_id, form.text)
@@ -67,13 +66,15 @@ class PostJsonController @Inject()(components: ControllerComponents)
 
             case None =>
               //Formに送信されたuser_idがuserテーブルに存在しなかった場合
-              BadRequest((Json.toJson(Response(Meta(400, s"user_id : ${form.user_id} not found")))))
+              BadRequest(
+                Json.obj("meta" -> (Json.toJson(Meta(400, s"user_id : ${form.user_id} not found")))))
           }
         }
       }
       .recoverTotal { e =>
         // Formが妥当で無い場合バリデーションエラーを返す
-        BadRequest(Json.obj("result" -> "Failure", "Error" -> JsError.toJson(e)))
+        BadRequest(
+          Json.obj("result" -> "Failure", "Error" -> JsError.toJson(e)))
       }
   }
 }
